9791d4a82803bb34f9b987d262ee716ada3943f6
Davi-Andrade-30 <84862726+Davi-Andrade-30@users.noreply.github.com>
2024-03-27 14:18:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -116,5 +116,14 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+function updatemenu() {
+    if (document.getElementById('responsive-menu').checked == true) {
+      document.getElementById('menu').style.borderBottomRightRadius = '0';
+      document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    }else{
+      document.getElementById('menu').style.borderRadius = '10px';
+    }
+  }
+
 
   
